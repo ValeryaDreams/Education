e77e9fc01f9870c7e11c1c036889d423d14ea98d
vvalerya <1997valerya1997@gmail.com>
2025-10-22 11:55:28 +0300

    Add example for OUT REF IN.

--- Education_Task_3/Education_Task_3/Program.cs
+++ Education_Task_3/Education_Task_3/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace Education_Task_3
+﻿using System.Xml;
+
+namespace Education_Task_3
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+        class Person
         {
-            Console.WriteLine("Hello, World!");
+                public string Name { get; set; }
+                public int Age { get; set; }
         }
-    }
+
+        internal class Program
+        {
+                static void Main(string[] args)
+                {                        
+                        Person p = new Person { Name = "Ivan", Age = 10 };
+                        Console.WriteLine($"Name: {p.Name} Age: {p.Age}");
+
+                        ChangePerson(p);
+                        Console.WriteLine($"Name: {p.Name} Age: {p.Age}");
+
+                        ChangePerson(ref p);
+                        Console.WriteLine($"Name: {p.Name} Age: {p.Age}");
+
+                        string text = "Hello";
+                        ChangeStr(text); 
+                        Console.WriteLine($"Change Text{text}");
+
+                        ChangeStr(ref text);
+                        Console.WriteLine($"Change Text{text}");
+
+                        Console.ReadKey();
+                }
+
+                static void ChangePerson(Person p)
+                {
+                        p = new Person { Name = "No Name" };
+                }
+
+                static void ChangePerson(ref Person p)
+                {
+                        p = new Person { Name = "No Name" };
+                }
+
+                static void ChangeStr(string newText)           // Link Copy.
+                {
+                        newText += "!";
+                }
+
+                static void ChangeStr(ref string newText)       // Original Link.
+                {
+                        newText += "!";
+                }
+        }
 }
