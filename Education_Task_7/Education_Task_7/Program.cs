c3b4a180cedeba3eb3accd0cf4fd9063fcd0bd28
vvalerya <1997valerya1997@gmail.com>
2025-11-24 15:44:22 +0300

    Example with Weak reference

--- Education_Task_7/Education_Task_7/Program.cs
+++ Education_Task_7/Education_Task_7/Program.cs
@@ -1,10 +1,53 @@
 ﻿namespace Education_Task_7
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+        record Person(string name, int age);
 
+        internal class Program
+        {
+                static void Main(string[] args)
+                {
+                        //string[] people = { "Zhenya", "ZKraken", "Olaya", "Valerya", "ZIvan" };
+
+                        //var selectedPeople = new List<string>();
+
+                        //foreach (var person in people)
+                        //{
+                        //        if (person.ToUpper().StartsWith("Z"))
+                        //        {
+                        //                selectedPeople.Add(person);
+                        //        }
+                        //}
+
+                        //selectedPeople.Sort();
+
+                        //var selectedPeople = from p in people
+                        //                     where p.ToUpper().StartsWith("Z")
+                        //                     orderby p
+                        //                     select p;
+
+                        //var selectedPeople = people.Where(p => p.ToUpper().StartsWith("Z")).OrderBy(p => p);
+
+                        //foreach (var person in selectedPeople)
+                        //{
+                        //        Console.WriteLine(person);
+                        //}
+
+                        var people = new List<Person>
+                        {
+                                new Person("Tom", 123 ),
+                                new Person("Tom", 123),
+                                new Person("Tom", 123)
+                        };
+
+                        var names = from p in people
+                                    select p.name;
+
+                        foreach (var p in names)
+                        {
+                                Console.WriteLine(p);
+                        }
+
+                        Console.ReadKey();
+                }
         }
-    }
 }
---
+++ Education_Task_9/Education_Task_9/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Education_Task_9
+{
+        internal class Program
+        {
+                public static WeakReference weak;
+
+                static void Main(string[] args)
+                {
+                        CreateObject();                      
+
+                        // Сильная ссылка obj существует => GC не может собрать объект.
+                        Console.WriteLine($"Alive: {weak.IsAlive}");
+
+                        // Принудительный запуск GC.
+                        // GC Смотрит:
+                        // сильной ссылки НЕТ.
+                        // слабая ссылка ЕСТЬ, но это не защита объекта => ОБЪЕКТ МОЖЕТ БЫТЬ УДАЛЕН.
+                        GC.Collect();
+
+                        Console.WriteLine($"Alive: {weak.IsAlive}");
+
+                        Console.ReadLine();
+                }
+
+                public static void CreateObject()
+                {
+                        // Сильная ссылка.
+                        // Пока эта ссылка существует — GC не имеет права удалить объект.
+                        var obj = new object();
+
+                        // Слабая ссылка.
+                        // WeakReference НЕ удерживает объект в памяти.
+                        // То есть объект жив только из-за сильной ссылки obj.
+                        weak = new WeakReference(obj);
+                }
+        }
+}
